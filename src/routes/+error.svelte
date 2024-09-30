03799358380ed63dd6509fbffcbc3dde3594267f
fanboykun <irfanramadhan1812@gmail.com>
2024-09-30 20:01:57 +0700

    add: fallback error page

---
+++ src/routes/+error.svelte
@@ -0,0 +1,18 @@
+<script lang="ts">
+	import { page } from '$app/stores';
+    const emojis = {
+        // TODO add the rest!
+        420: '🫠',
+        500: '💥'
+    } as const
+
+    const getEmoji = (status: number) => {
+        return emojis[status as keyof typeof emojis] ?? emojis[500]
+    }
+    
+</script>
+
+<h1>{$page.status} {$page?.error?.message}</h1>
+<span style="font-size: 10em">
+	{getEmoji($page.status)}
+</span>
